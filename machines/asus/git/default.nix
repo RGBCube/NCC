90faaf6b28f64cf8926f002da6793a3ae44aff22
RGBCube <RGBCube@users.noreply.github.com>
2023-05-12 23:23:00 +0300

    Move git aliases to git/

--- machines/asus/git/default.nix
+++ machines/asus/git/default.nix
@@ -1,6 +1,19 @@
 { homeConfiguration, enabled, ... }:
 
 homeConfiguration "nixos" {
+  programs.nushell.shellAliases = {
+    g = "git";
+    ga = "git add ./";
+    gb = "git rebase -i";
+    gc = "git commit -m";
+    gca = "git commit --amend";
+    gcl = "git clone";
+    gd = "git diff";
+    gds = "git diff --staged";
+    gp = "git push";
+    gs = "git status";
+  };
+
   programs.git = enabled {
     userName = "RGBCube";
     userEmail = "RGBCube@users.noreply.github.com";
--- machines/asus/nushell/default.nix
+++ machines/asus/nushell/default.nix
@@ -19,17 +19,6 @@
     envFile.source = ./environment.nu;
 
     shellAliases = {
-      g = "git";
-      ga = "git add ./";
-      gb = "git rebase -i";
-      gc = "git commit -m";
-      gca = "git commit --amend";
-      gcl = "git clone";
-      gd = "git diff";
-      gds = "git diff --staged";
-      gp = "git push";
-      gs = "git status";
-
       la = "ls -a";
       sl = "ls";
 
