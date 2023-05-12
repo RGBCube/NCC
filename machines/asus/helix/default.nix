8c5379fb222aa12880e115e3983dcdaaec012df4
RGBCube <RGBCube@users.noreply.github.com>
2023-05-12 22:04:04 +0300

    Add Helix

---
+++ machines/asus/helix/default.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, systemConfiguration, homeConfiguration, packages, enabled, projectPath, ... }:
+
+homeConfiguration "nixos" {
+  programs.helix = enabled {
+    settings.editor = {
+      auto-pairs."<" = ">";
+      auto-pairs."`" = "`";
+      auto-save = true;
+      color-modes = true;
+      cursor-shape.normal = "bar";
+      cursorline = true;
+      file-picker.hidden = false;
+      indent-guides.render = true;
+      indent-guides.character = "▏";
+      indent-guides.skip-levels = 1;
+      line-number = "relative";
+      shell = [ "nu" "-c" ];
+      text-width = 100;
+      whitespace.render.space = "all";
+      whitespace.render.tab = "all";
+      whitespace.characters.space = "p";
+      whitespace.characters.tab = "→";
+    };
+  };
+}
