7b18ac23c73409df5812a97e91c94f6d035dd8c6
RGBCube <RGBCube@users.noreply.github.com>
2023-05-24 21:00:33 +0300

    Add catppuccin theme for Bat

--- machines/asus/bat/default.nix
+++ machines/asus/bat/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, systemPackages, homeConfiguration, ... }: lib.recursiveUpdate
+{ lib, pkgs, systemPackages, homeConfiguration, enabled, ... }: lib.recursiveUpdate
 
 (with pkgs; systemPackages [
   bat
@@ -8,5 +8,16 @@
   programs.nushell.environmentVariables = {
     PAGER = "\"bat --plain\"";
     MANPAGER = "\"sh -c 'col --spaces --no-backspaces | bat --plain --language man'\"";
+  };
+
+  programs.bat = enabled {
+    config.theme = "catppuccin";
+
+    themes.catppuccin = builtins.readFile (pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "bat";
+      rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+      sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+    } + "/Catppuccin-mocha.tmTheme");
   };
 })
