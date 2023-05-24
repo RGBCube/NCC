6e9a189d88009c314feb8a41a9e30d3c58fe7894
RGBCube <RGBCube@users.noreply.github.com>
2023-05-24 20:38:54 +0300

    Set bat as pager only in home

--- machines/asus/bat/default.nix
+++ machines/asus/bat/default.nix
@@ -1,11 +1,11 @@
-{ lib, pkgs, systemConfiguration, systemPackages, ... }: lib.recursiveUpdate
+{ lib, pkgs, systemPackages, homeConfiguration, ... }: lib.recursiveUpdate
 
 (with pkgs; systemPackages [
   bat
 ])
 
-(systemConfiguration {
-  environment.sessionVariables = {
+(homeConfiguration "nixos" {
+  programs.nushell.environmentVariables = {
     PAGER = "bat --plain";
     MANPAGER = "sh -c 'col --spaces --no-backspaces | bat --plain --language man'";
   };
