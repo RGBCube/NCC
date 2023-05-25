6745fa9bbacb4b2819d0efebeb41ccb1f8c78c3a
RGBCube <RGBCube@users.noreply.github.com>
2023-05-25 17:46:59 +0300

    Adjust git aliases

--- machines/asus/git/default.nix
+++ machines/asus/git/default.nix
@@ -3,16 +3,20 @@
 homeConfiguration "nixos" {
   programs.nushell.shellAliases = {
     g    = "git";
-    ga   = "git add ./";
-    grb  = "git rebase --interactive";
+    ga   = "git add";
+    gaa  = "git add ./";
+    grb  = "git rebase";
+    grbi = "git rebase --interactive";
     grba = "git rebase --abort";
-    gc   = "git commit --message";
-    gca  = "git commit --amend";
+    gc   = "git commit";
+    gcm  = "git commit --message";
+    gca  = "git commit --amend --no-edit";
     gcl  = "git clone";
     gd   = "git diff";
     gds  = "git diff --staged";
     gp   = "git push";
     gs   = "git stash";
+    gsp  = "git stash pop";
     gst  = "git status";
   };
 
