27065ad008c912030aed466b3bf5d6d62a86e943
RGBCube <RGBCube@users.noreply.github.com>
2023-05-27 21:06:09 +0300

    Add venv alias again

--- machines/asus/python/default.nix
+++ machines/asus/python/default.nix
@@ -1,10 +1,19 @@
-{ pkgs, systemPackages, ... }:
+{ lib, pkgs, systemPackages, homeConfiguration, ... }: lib.recursiveUpdate
 
-with pkgs; systemPackages [
+(with pkgs; systemPackages [
   (python311.withPackages (pkgs: with pkgs; [
     pip
     requests
   ]))
   virtualenv
   poetry
-]
+])
+
+(homeConfiguration "nixos" {
+  programs.nushell.envFile.source = ''
+    def venv [] {
+      virtualenv venv
+      source ./venv/bin/activate.nu
+    }
+  '';
+})
