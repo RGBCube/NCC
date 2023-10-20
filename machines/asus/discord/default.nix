fb676ff36b4dff855b3b54579fb24263df80927b
RGBCube <RGBCube@users.noreply.github.com>
2023-10-20 16:35:09 +0300

    Use Discord with Vencord

--- machines/asus/discord/default.nix
+++ machines/asus/discord/default.nix
@@ -1,7 +1,8 @@
 { pkgs, homePackages, ... }:
 
 with pkgs; homePackages "nixos" [
-    (discord.override {
-      withOpenASAR = true;
-    })
+  (discord.override {
+    withOpenASAR = true;
+    withVencord  = true;
+  })
 ]
