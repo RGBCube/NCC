3295ed141307db61596b76a7e0608063609b10e3
RGBCube <RGBCube@users.noreply.github.com>
2023-10-21 11:48:39 +0300

    Add UUIDs

--- machines/enka/hardware.nix
+++ machines/enka/hardware.nix
@@ -18,18 +18,18 @@ systemConfiguration {
   };
 
   fileSystems."/" = {
-    device = "";
+    device = "/dev/disk/by-uuid/add562e1-f2f6-4ac0-bb17-0acc380ac133";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "";
+    device = "/dev/disk/by-uuid/D697-1FA1";
     fsType = "vfat";
   };
 
   swapDevices = [
     {
-      device = "";
+      device = "/dev/disk/by-uuid/7b21b5e9-6194-4448-9f65-3a744bfdc2b3";
     }
   ];
 }
