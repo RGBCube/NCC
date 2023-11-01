88f5eb85510597d4df2a9d313db7a0c6efe56db4
RGBCube <RGBCube@users.noreply.github.com>
2023-11-01 19:19:39 +0300

    Empty FS UUIDs

--- machines/enka/hardware.nix
+++ machines/enka/hardware.nix
@@ -18,18 +18,18 @@ systemConfiguration {
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/add562e1-f2f6-4ac0-bb17-0acc380ac133";
+    device = "";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D697-1FA1";
+    device = "";
     fsType = "vfat";
   };
 
   swapDevices = [
     {
-      device = "/dev/disk/by-uuid/7b21b5e9-6194-4448-9f65-3a744bfdc2b3";
+      device = "";
     }
   ];
 }
