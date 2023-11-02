66427b2be42bae6852f5fcead6754b0b6becfa90
RGBCube <RGBCube@users.noreply.github.com>
2023-11-02 14:34:42 +0300

    Add hardware IDs

--- machines/enka/hardware.nix
+++ machines/enka/hardware.nix
@@ -8,7 +8,7 @@ systemConfiguration {
     "rtsx_pci_sdmmc"
     "sd_mod"
     "sr_mod"
-    "usb_storage"
+    "usbhid"
     "xhci_pci"
   ];
 
@@ -18,18 +18,18 @@ systemConfiguration {
   };
 
   fileSystems."/" = {
-    device = "";
+    device = "/dev/disk/by-uuid/cc473c12-94a9-4399-a0ab-f080f9e786f6";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "";
+    device = "/dev/disk/by-uuid/D1F5-D862";
     fsType = "vfat";
   };
 
   swapDevices = [
     {
-      device = "";
+      device = "/dev/disk/by-uuid/f3a15cd2-9897-4867-9414-d4a8c3e71caf";
     }
   ];
 }
