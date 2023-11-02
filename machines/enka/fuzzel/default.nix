49c20827a139fe582610d913a40d3c7ac89f9400
RGBCube <RGBCube@users.noreply.github.com>
2023-11-02 14:35:34 +0300

    Fix fuzzel config

--- machines/enka/fuzzel/default.nix
+++ machines/enka/fuzzel/default.nix
@@ -2,27 +2,26 @@
 
 homeConfiguration "nixos" {
   programs.fuzzel = enabled {
-    settings = ''
-      font = JetBrainsMono:size=12
-      prompt = "❯ "
+    settings.main = {
+      font   = "JetBrainsMono:size=12";
+      prompt = "❯ ";
 
-      terminal = kitty
+      terminal = "kitty";
 
-      tabs = 4
+      tabs = 4;
 
-      horizontal-pad = 10
-      vertical-pad   = 10
-      inner-pad      = 5
+      horizontal-pad = 10;
+      vertical-pad   = 10;
+      inner-pad      = 5;
 
-      background-color = 1D2021
-      text             = DDC7A1
-      match            = D79921
-      selection        = DDC7A1
-      selection-text   = 1D2021
-      border = D79921
+      background-color = "1D2021";
+      text             = "DDC7A1";
+      match            = "D79921";
+      selection        = "DDC7A1";
+      selection-text   = "1D2021";
+      border           = "D79921";
+    };
 
-      [border]
-      radius = 0
-    '';
+    settings.border.radius = 0;
   };
 }
