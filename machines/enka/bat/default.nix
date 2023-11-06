89fb6b66fad3e9e977a0463fb3a68a79334010d2
RGBCube <RGBCube@users.noreply.github.com>
2023-11-06 11:48:53 +0300

    Change Bat theme to Gruvbox

--- machines/enka/bat/default.nix
+++ machines/enka/bat/default.nix
@@ -11,13 +11,6 @@
   };
 
   programs.bat = enabled {
-    config.theme = "catppuccin";
-
-    themes.catppuccin = builtins.readFile (pkgs.fetchFromGitHub {
-      owner  = "catppuccin";
-      repo   = "bat";
-      rev    = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-      sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-    } + "/Catppuccin-mocha.tmTheme");
+    config.theme = "gruvbox-dark";
   };
 })
