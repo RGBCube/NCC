d23b5fa9da60256deed2654e44feb1f91d5fabed
RGBCube <RGBCube@users.noreply.github.com>
2023-11-10 11:50:08 +0300

    Add screenshot notifications

--- machines/enka/hyprland/default.nix
+++ machines/enka/hyprland/default.nix
@@ -66,8 +66,8 @@
       bind = SUPER, W,      exec, firefox
       bind = SUPER, D,      exec, discord
 
-      bind =      , PRINT, exec, grim -g "$(slurp -c 00000000)" - | wl-copy --type image/png
-      bind = SHIFT, PRINT, exec, grim                           - | wl-copy --type image/png
+      bind =      , PRINT, exec, grim -g "$(slurp -c 00000000)" - | wl-copy --type image/png; dunstify --timeout 1000 "Screenshot Copied To Clipboard"
+      bind = SHIFT, PRINT, exec, grim                           - | wl-copy --type image/png; dunstify --timeout 1000 "Screenshot Copied To Clipboard"
       bind = CTRL,  PRINT, exec, kazam
 
       binde = , XF86AudioRaiseVolume, exec, wpctl set-volume --limit 1.5 @DEFAULT_AUDIO_SINK@ 5%+; /home/nixos/.config/hypr/volume.sh
