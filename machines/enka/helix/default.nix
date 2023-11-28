cef55777d1a65145f08e9c17fde25d62825cf46b
RGBCube <RGBCube@users.noreply.github.com>
2023-11-28 11:04:53 +0300

    Simplify Helix config

--- machines/enka/helix/default.nix
+++ machines/enka/helix/default.nix
@@ -1,4 +1,4 @@
-{ lib, ulib, pkgs, homeConfiguration, homePackages, enabled, ... }: lib.recursiveUpdate
+{ lib, pkgs, homeConfiguration, homePackages, enabled, ... }: lib.recursiveUpdate
 
 (homeConfiguration [ "nixos" "root" ] {
   programs.nushell.environmentVariables.EDITOR = "hx";
@@ -35,23 +35,9 @@
       render.tab     = "all";
     };
 
-    settings.keys = lib.recursiveUpdate
-
-    (ulib.recursiveUpdateMap (mode: { ${mode} = {
-      C-h = "move_prev_word_start";
-      C-l = "move_next_word_end";
-      C-k = "move_visual_line_up";
-      C-j = "move_visual_line_down";
-
-      C-left  = "move_prev_word_start";
-      C-right = "move_next_word_end";
-      C-up    = "move_visual_line_up";
-      C-down  = "move_visual_line_down";
-    }; }) [ "insert" "normal" "select" ])
-
-    (ulib.recursiveUpdateMap (mode: {
-      ${mode}.D = "extend_to_line_end";
-    }) [ "normal" "select" ]);
+    settings.keys = lib.genAttrs [ "normal" "select" ] (_: {
+      D = "extend_to_line_end";
+    });
   };
 })
 
