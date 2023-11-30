6ded385f22d652a1d879f13b224197de5a1adedf
RGBCube <RGBCube@users.noreply.github.com>
2023-12-01 01:00:20 +0300

    Use cage

--- machines/enka/greetd/default.nix
+++ machines/enka/greetd/default.nix
@@ -1,34 +1,9 @@
-{ pkgs, upkgs, systemConfiguration, enabled, ... }:
+{ pkgs, systemConfiguration, enabled, ... }:
 
-let
-  hyprlandBin = "${upkgs.hyprland}/bin";
-
-  hyprlandConfig = pkgs.writeText "hyprland.conf" ''
-    animations {
-      enabled                = 0
-      first_launch_animation = 0
-    }
-
-    input {
-      kb_layout = tr
-    }
-
-    misc {
-      force_default_wallpaper = 0
-    }
-
-    workspace = 1, default: true, gapsout: 0, gapsin: 0, border: false, decorate: false
-
-    exec-once = systemctl --user stop waybar.service
-
-    exec-once = [workspace 1; fullscreen; noanim] ${pkgs.greetd.gtkgreet}/bin/gtkgreet --layer-shell --command Hyprland; ${hyprlandBin}}/bin/hyprctl dispatch exit
-
-    exec-once = ${hyprlandBin}/bin/hyprctl dispatch focuswindow gtkgreet
-  '';
-in systemConfiguration {
+systemConfiguration {
   services.greetd = enabled {
     settings.default_session = {
-      command = "${hyprlandBin}/Hyprland --config ${hyprlandConfig}";
+      command = "${pkgs.cage}/bin/cage -sd ${pkgs.greetd.gtkgreet}/bin/gtkgreet";
       user    = "nixos";
     };
   };
