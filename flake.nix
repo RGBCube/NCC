a0fa73b873a0b5e5cc54c4ecfe77ab8b3195945a
RGBCube <RGBCube@users.noreply.github.com>
2023-12-10 21:11:57 +0300

    Move commented code to BROKEN.md

--- flake.nix
+++ flake.nix
@@ -1,35 +1,6 @@
 {
   description = "All my NixOS configurations.";
 
-  # nixConfig = with builtins; mapAttrs (_: concatStringsSep " ") {
-  #   extra-substituters = [
-  #     "https://nix-community.cachix.org/"
-  #     "https://hyprland.cachix.org/"
-  #     "https://cache.privatevoid.net/"
-  #   ];
-
-  #   extra-trusted-public-keys = [
-  #     "nix-community.cachix.org:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  #     "hyprland.cachix.org:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-  #     "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
-  #   ];
-  # };
-
-  # inputs = with builtins; mapAttrs (name: url: {
-  #   url = "git+https://${url}";
-  # } // (if elem name [ "nixSuper" "hyprland" "hyprpicker" ] then {} else {
-  #   inputs.nixpkgs.follows = "nixpkgs";
-  # })) {
-  #   nixSuper    = "github.com/privatevoid-net/nix-super";
-  #   nixpkgs     = "github.com/NixOS/nixpkgs/tree/nixos-unstable";
-  #   homeManager = "github.com/nix-community/home-manager";
-  #   hyprland    = "github.com/hyprwm/Hyprland";
-  #   hyprpicker  = "github.com/hyprwm/hyprpicker";
-  #   fenix       = "github.com/nix-community/fenix";
-  #   tools       = "github.com/RGBCube/FlakeTools";
-  #   themes      = "github.com/RGBCube/ThemeNix";
-  # };
-
   nixConfig = {
     extra-substituters = ''
       https://nix-community.cachix.org/
