2613e4af27afd0cf36635e945fb1f9048cb53bf0
RGBCube <RGBCube@users.noreply.github.com>
2023-12-16 11:13:53 +0300

    Apply setEnv for all hosts

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -2,11 +2,12 @@
 
 homeConfiguration {
   programs.ssh = enabled {
+    matchBlocks."*".setEnv.TERM  = "xterm-kitty";
+
     matchBlocks.robotic = {
       hostname     = "86.105.252.189";
       user         = "rgbcube";
       port         = 2299;
-      setEnv.TERM  = "xterm-kitty";
       identityFile = "~/.ssh/id_rsa";
     };
   };
