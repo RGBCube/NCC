8dd0b9bbc60de0557460ad25f7198497a79ae919
RGBCube <RGBCube@users.noreply.github.com>
2024-01-10 15:12:26 +0300

    Add cube id_ed25519 to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ with import ./keys.nix;
 {
   "acme.age".publicKeys                = [ rgbcube cube ];
   "cube.password.age".publicKeys       = [ rgbcube cube ];
+  "cube.id.age".publicKeys             = [ rgbcube ];
   "enka.said.password.age".publicKeys  = [ rgbcube ];
   "enka.orhan.password.age".publicKeys = [ rgbcube ];
 }
