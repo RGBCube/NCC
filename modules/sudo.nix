a1aca8a0d3080ceea139d44f96850dd1c97293a5
RGBCube <git@rgbcu.be>
2024-01-15 15:52:38 +0300

    Upgrade sudo config

--- modules/sudo.nix
+++ modules/sudo.nix
@@ -1,10 +1,56 @@
-{ ulib, ... }: with ulib;
+{ lib, ulib, ... }: with ulib; merge
 
-systemConfiguration {
+(desktopSystemConfiguration {
+  security.sudo.wheelNeedsPassword = false;
+})
+
+(systemConfiguration {
   security.sudo = enabled {
     extraConfig   = ''
-      Defaults timestamp_timeout=${if ulib.isDesktop then "-1" else "0"}
+      Defaults lecture = never
+      Defaults pwfeedback
+      Defaults env_keep += "DISPLAY EDITOR PATH"
+      ${lib.optionalString ulib.isServer ''
+        Defaults timestamp_timeout = 0
+      ''}
     '';
     execWheelOnly = true;
+
+    extraRules = [{
+      groups   = [ "wheel" ];
+      commands = let
+        system = "/run/current-system";
+        store = "/nix/store";
+      in [
+        {
+          command = "${store}/*/bin/switch-to-configuration";
+          options = [ "SETENV" "NOPASSWD" ];
+        }
+        {
+          command = "${system}/sw/bin/nix system";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${system}/sw/bin/nix-collect-garbage";
+          options = [ "SETENV" "NOPASSWD" ];
+        }
+        {
+          command = "${system}/sw/bin/nix-env";
+          options = [ "SETENV" "NOPASSWD" ];
+        }
+        {
+          command = "${system}/sw/bin/nix-store";
+          options = [ "SETENV" "NOPASSWD" ];
+        }
+        {
+          command = "${system}/sw/bin/nixos-rebuild";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${system}/sw/bin/systemctl";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }];
   };
-}
+})
