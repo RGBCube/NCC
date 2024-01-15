9c91731341e7908befc829283064aee739f63c42
RGBCube <git@rgbcu.be>
2024-01-15 13:20:37 +0300

    Add mail to nextcloud

--- hosts/cube/nextcloud.nix
+++ hosts/cube/nextcloud.nix
@@ -10,6 +10,11 @@ in serverSystemConfiguration {
     group = "nextcloud";
   };
 
+  age.secrets."cube/password.mail.nextcloud" = {
+    owner = "nextcloud";
+    group = "nextcloud";
+  };
+
   services.nextcloud = enabled {
     package  = pkgs.nextcloud28;
 
@@ -21,16 +26,19 @@ in serverSystemConfiguration {
     config.adminuser     = "admin";
     config.adminpassFile = config.age.secrets."cube/password.nextcloud".path;
 
-    config.dbtype          = "pgsql";
+    config.dbhost = "/run/postgresql";
+    config.dbtype = "pgsql";
 
-    extraAppsEnable = true;
-    extraApps       = {
-      inherit (config.services.nextcloud.package.packages.apps)
-        bookmarks calendar contacts deck 
-        forms groupfolders impersonate
-        mail maps notes phonetrack
-        polls previewgenerator tasks;
-        # Add: files_markdown files_texteditor memories news
+    secretFile = config.age.secrets."cube/password.mail.nextcloud".path;
+    extraOptions = {
+      mail_domain   = domain;
+      mail_smtphost = domain;
+
+      mail_from_address = "cloud";
+      mail_smtpname     = "contact";
+
+      mail_smtpauth   = true;
+      mail_smtpsecure = "ssl";
     };
 
     extraOptions.enabledPreviewProviders = [
@@ -46,6 +54,16 @@ in serverSystemConfiguration {
       "OC\\Preview\\XBitmap"
       "OC\\Preview\\HEIC"
     ];
+
+    extraAppsEnable = true;
+    extraApps       = {
+      inherit (config.services.nextcloud.package.packages.apps)
+        bookmarks calendar contacts deck
+        forms groupfolders impersonate
+        mail maps notes phonetrack
+        polls previewgenerator tasks;
+        # Add: files_markdown files_texteditor memories news
+    };
 
     nginx.recommendedHttpHeaders = true;
   };
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,16 +4,19 @@ rec {
     cube     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMkCJeHcD0SIOZ4HkyF6rqUmbvlKhSha3HWMZ0hbIjp rgb@cube";
   };
 
-  "cube/id.age".publicKeys                    = [ keys.rgbcube ];
+  "cube/id.age".publicKeys = [ keys.rgbcube ];
 
-  "cube/password.hash.mail.age".publicKeys    = [ keys.cube ];
-  "cube/password.hash.rgb.age".publicKeys     = [ keys.cube ];
+  "cube/password.hash.mail.age".publicKeys = [ keys.cube ];
+  "cube/password.hash.rgb.age".publicKeys  = [ keys.cube ];
 
-  "cube/password.acme.age".publicKeys         = [ keys.cube ];
-  "cube/password.grafana.age".publicKeys      = [ keys.cube ];
-  "cube/password.mail.grafana.age".publicKeys = [ keys.cube ];
-  "cube/password.nextcloud.age".publicKeys    = [ keys.cube ];
+  "cube/password.acme.age".publicKeys = [ keys.cube ];
 
-  "enka/password.hash.orhan.age".publicKeys   = [ keys.rgbcube ];
-  "enka/password.hash.said.age".publicKeys    = [ keys.rgbcube ];
+  "cube/password.mail.grafana.age".publicKeys   = [ keys.cube ];
+  "cube/password.mail.nextcloud.age".publicKeys = [ keys.cube ];
+
+  "cube/password.grafana.age".publicKeys   = [ keys.cube ];
+  "cube/password.nextcloud.age".publicKeys = [ keys.cube ];
+
+  "enka/password.hash.orhan.age".publicKeys = [ keys.rgbcube ];
+  "enka/password.hash.said.age".publicKeys  = [ keys.rgbcube ];
 }
