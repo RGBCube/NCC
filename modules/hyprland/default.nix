17f3c952e708779571c0b85c3d5a241ab765b1af
RGBCube <git@rgbcu.be>
2024-01-18 08:01:24 +0300

    Add Element alais

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -104,6 +104,7 @@
       bind = SUPER    , RETURN, exec, ghostty --gtk-single-instance=true
       bind = SUPER    , W     , exec, firefox
       bind = SUPER    , D     , exec, discordcanary
+      bind = SUPER    , E     , exec, element-desktop
       bind = SUPER    , M     , exec, thunderbird
       bind = SUPER    , T     , exec, thunar
       bind = SUPER    , C     , exec, hyprpicker --autocopy
