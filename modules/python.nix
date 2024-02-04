15cd94166b094cfc114627ca780d3caa97e396fe
RGBCube <git@rgbcu.be>
2024-02-04 22:44:57 +0300

    Downgrade to python 311

--- modules/python.nix
+++ modules/python.nix
@@ -1,12 +1,12 @@
 { ulib, pkgs, ... }: with ulib; merge
 
 (systemPackages (with pkgs; [
-  (python312.withPackages (pkgs: with pkgs; [
+  (python311.withPackages (pkgs: with pkgs; [
     pip
     requests
   ]))
   virtualenv
-  # poetry # Broken for some reason ATM.
+  poetry
 ]))
 
 (homeConfiguration {
