8808152df7fcee652f8587bad223530fdc6d1580
RGBCube <git@rgbcu.be>
2024-04-19 16:39:42 +0300

    Add 1.1.1.1 and 8.8.8.8 as backup nameservers

--- modules/kresd.nix
+++ modules/kresd.nix
@@ -3,5 +3,11 @@
 systemConfiguration {
   services.kresd = enabled;
 
-  networking.nameservers = [ "::1" "127.0.0.1" ];
+  networking.nameservers = [
+    "::1"
+    "127.0.0.1"
+
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
 }
