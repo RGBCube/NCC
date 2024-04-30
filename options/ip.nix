91ee04c46f9a049204ba5eefe8c50d040178df6c
RGBCube <git@rgbcu.be>
2024-04-30 12:56:08 +0300

    Bettee grouping of options

--- options/ip.nix
+++ options/ip.nix
@@ -1,4 +1,6 @@
 { lib, ... }: {
-  options.networking.ipv4 = lib.mkValue null;
-  options.networking.ipv6 = lib.mkValue null;
+  options.networking = {
+    ipv4 = lib.mkValue null;
+    ipv6 = lib.mkValue null;
+  };
 }
