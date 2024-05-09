ecee56ae027d302d9c8a6cb5fac7ab2c66abe177
RGBCube <git@rgbcu.be>
2024-05-09 23:29:04 +0300

    Add tailscale domain search suffix to resolved

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -4,6 +4,8 @@ let
   # Shorter is better for networking interfaces IMO.
   interface = "ts0";
 in systemConfiguration {
+  services.resolved.domains = [ "warthog-major.ts.net" ];
+
   services.tailscale = enabled {
     interfaceName      = interface;
     useRoutingFeatures = "both";
