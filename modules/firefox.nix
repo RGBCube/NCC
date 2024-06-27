5819599d11dcbb86635c6ee0dabea5a0d485094d
RGBCube <git@rgbcu.be>
2024-06-27 10:27:02 +0300

    Hide tab bar in Firefox

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -2,14 +2,24 @@
 
 desktopUserHomeConfiguration {
   programs.firefox = enabled {
-    profiles.default.settings = with config.theme.font; {
-      "general.autoScroll"               = true;
-      "privacy.donottrackheader.enabled" = true;
+    profiles.default = {
+      settings = with config.theme.font; {
+        "general.autoScroll"               = true;
+        "privacy.donottrackheader.enabled" = true;
 
-      "browser.fixup.domainsuffixwhitelist.idk" = true;
+        "browser.fixup.domainsuffixwhitelist.idk" = true;
 
-      "font.name.serif.x-western"    = sans.name;
-      "font.size.variable.x-western" = builtins.ceil (1.3 * size.normal);
+        "font.name.serif.x-western"    = sans.name;
+        "font.size.variable.x-western" = builtins.ceil (1.3 * size.normal);
+
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+
+      userChrome = ''
+        #TabsToolbar {
+          visibility: collapse;
+        }
+      '';
     };
   };
 }
