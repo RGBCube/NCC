64e2dc5f719b5ca2defd1b5ff978b2dc9e59afb4
RGBCube <git@rgbcu.be>
2025-01-24 23:50:51 +0300

    Move collect to inside lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,6 @@
 inputs: self: super: let
-  option = import ./option.nix inputs self super;
-  system = import ./system.nix inputs self super;
-  values = import ./values.nix inputs self super;
-in option // system // values
+  filesystem = import ./filesystem.nix inputs self super;
+  option     = import ./option.nix     inputs self super;
+  system     = import ./system.nix     inputs self super;
+  values     = import ./values.nix     inputs self super;
+in filesystem // option // system // values
