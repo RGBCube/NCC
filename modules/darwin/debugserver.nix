a7a0d9296f8bd2f2712ec523507b69514a8ebe82
RGBCube <git@rgbcu.be>
2025-02-09 18:28:32 +0300

    Set LLDB debugserver path

---
+++ modules/darwin/debugserver.nix
@@ -0,0 +1,4 @@
+{
+  # <https://sourcegraph.com/github.com/llvm/llvm-project@70906f0514826b5e64bd9354210ae836740c2053/-/blob/lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunication.cpp?L883>
+  environment.variables.LLDB_DEBUGSERVER_PATH = "/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Versions/A/Resources/debugserver";
+}
