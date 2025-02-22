c79856ecf79a5094b6e2920acdf609532b669a72
RGBCube <git@rgbcu.be>
2025-02-22 23:59:20 +0300

    feat: add privatevoid cachers

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.garnix.io/"
+      "https://cache.privatevoid.net"
       "https://hyprland.cachix.org/"
       "https://nix-community.cachix.org/"
     ];
 
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
