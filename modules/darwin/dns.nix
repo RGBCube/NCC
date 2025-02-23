0196b7812af213ab74d448d7ec8dd6a8561892d9
RGBCube <git@rgbcu.be>
2025-02-23 15:18:14 +0300

    fix: fix dns servers option

--- modules/darwin/dns.nix
+++ modules/darwin/dns.nix
@@ -3,7 +3,7 @@
 in {
   # Yeah, no DNSSEC or DoT or anything.
   # That's what you get for using Darwin I guess.
-  networking.dns = config.networking.dns.servers
+  networking.dns = config.dns.servers
     |> map (splitString "#")
     |> map head;
 
