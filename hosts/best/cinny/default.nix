0e70f7c08d7ec78eaecdcd0e9c9222ecd3dac57f
RGBCube <git@rgbcu.be>
2025-06-15 00:26:39 +0300

    cinny: use .patch instead of whole repo

--- hosts/best/cinny/default.nix
+++ hosts/best/cinny/default.nix
@@ -39,57 +39,10 @@
   };
 in {
   nixpkgs.overlays = [(self: super: {
-    cinny-unwrapped = flip self.callPackage {} ({
-      lib,
-      buildNpmPackage,
-      fetchFromGitHub,
-      giflib,
-      python3,
-      pkg-config,
-      pixman,
-      cairo,
-      pango,
-      stdenv,
-    }:
-
-    buildNpmPackage {
-      pname = "cinny";
-      version = "4.8.0";
-
-      src = fetchFromGitHub {
-        owner = "RGBCube";
-        repo  = "cinny";
-        rev   = "becc5f65820c6bf0d9acf3ddf5519519c3e174ad";
-        hash  = "sha256-Ym7BzkWjwR+ojP5jGBeHJeH03PZFuiME54RILR7pDqs=";
-      };
-
-      npmDepsHash = "sha256-LZLaaFL7vmFos3TCL4brT6gyEpZFjctsag6uH4CQPdI=";
-
-      nativeBuildInputs = [
-        python3
-        pkg-config
+    cinny-unwrapped = super.cinny-unwrapped.overrideAttrs (old: {
+      patches = old.patches or [] ++ [
+        ./all-styles.patch
       ];
-
-      buildInputs = [
-        pixman
-        cairo
-        pango
-      ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ giflib ];
-
-      installPhase = ''
-        runHook preInstall
-
-        cp -r dist $out
-
-        runHook postInstall
-      '';
-
-      meta = {
-        description = "Yet another Matrix client for the web";
-        homepage    = "https://cinny.in/";
-        license     = lib.licenses.agpl3Only;
-        platforms   = lib.platforms.all;
-      };
     });
   })];
 
